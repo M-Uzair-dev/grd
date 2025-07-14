821a6e0d11142b82601b21cc67a4fa7bdd90ec24
Uzair Manan <uzairmanan3@gmail.com>
2025-07-14 13:57:52 +0500

    Fixed the cors issue

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 // Handle preflight OPTIONS requests for all routes
 app.options('*', cors());
